cd0fe36be2b45bfe74d0462da4bb4cc5a36e54c6
Floremila <flormequi@gmail.com>
2025-02-21 11:07:24 +0100

    Moved instructions below the input field

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,9 +29,7 @@ const Home: React.FC = () => {
     <div className="centered">
       <h1>Welcome to CatPlace</h1>
 
-    
       <p className="question">What’s your name?</p>
-
       <input
         type="text"
         placeholder="Enter your name"
@@ -40,15 +38,22 @@ const Home: React.FC = () => {
         className="input-field"
       />
 
-      <p>
-        <strong>{user.name}</strong> has seen {user.catsSeen} cats today.
+      {/* Mover el texto aquí */}
+      <p className="instructions">
+        Click the button for each homeless cat you have seen today.
       </p>
 
       <button onClick={handleSeeCat} className="cat-button">
         I've seen a cat!
       </button>
+
+      <p>
+        <strong>{user.name}</strong> has seen {user.catsSeen} cats today.
+      </p>
     </div>
 );
+
+
 
 };
 
